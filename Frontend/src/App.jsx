e2d19b3aac4fb15e4de51b6e5f81307880601257
Mohd Abdul Mannan <abdulmannan@Mohds-MacBook-Air.local>
2024-03-24 13:19:42 +0530

    demo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,38 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const arr1 = ['Hi', 'Heared the News']
-  arr1.forEach(element => {
-    Messenger1(element)
-  });
+// const arr1 = ['Hi', 'Heared the News']
+//   arr1.forEach(element => {
+//     Messenger1(element)
+//   });
 
-const arr2 = ['Hello', 'No what happened']
-  arr2.forEach(element =>{
-    Messenger2(element)
-  })
+// const arr2 = ['Hello', 'No what happened']
+//   arr2.forEach(element =>{
+//     Messenger2(element)
+//   })
 
-function Messenger1(prop) {
-  return (
-    <div className='Text'>{prop}</div>
-  )
-}
+// function Messenger1(prop) {
+//   return (
+//     <div className='Text'>{prop}</div>
+//   )
+// }
 
-function Messenger2(prop) {
-  return (
-    <div className='Text'>{prop}</div>
-  )
-}
-
+// function Messenger2(prop) {
+//   return (
+//     <div className='Text'>{prop}</div>
+//   )
+// }
 function App() {
+  let message = "";
+  const [messages, setMessages] = useState([]);
+  function handleButton() {
+    let newArray = [...messages, message];
+    setMessages(newArray);
+  }
+
   return (
     <>
-      <div className='chats'>
-        <Messenger1 />
-        <Messenger2 />
-      </div>
+      {messages.map((element => <p>{element}</p>))}
       <div className='sendbox'>
-        <input type="text" placeholder='type your message here'/>
-        <button className='sendbtn'>Send</button>
+        <input type="text" placeholder='type your message here' onInput={(e) => { 
+          message = e.target.value;
+          }}/>
+        <button className='sendbtn' onClick = {handleButton}>Send</button>
       </div>
     </>
   )
