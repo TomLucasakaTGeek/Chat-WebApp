9a4bdbe015f8ceaad5e7acc72d814ab8668d46ee
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-03-27 10:19:20 +0530

    Renaming Login Page directory to Pages for various pages and transferring message logic to Messaging Page sub-directory

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,35 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LoginPage } from './Login Page/Login.jsx'
+import { LoginPage } from './Pages/Login Page/Login.jsx'
+import { Message } from './Pages/Messaging Page/Message.jsx'
 
-function Message() {
-  let message = "";
-  const [messages, setMessages] = useState([]);
-  function handleButton() {
-    let newArray = [...messages, message];
-    setMessages(newArray);
-  }
-
-  return (
-  <>
-    <div className='chats'>
-      {messages.map((element => <p id='msg'>{element}</p>))}
-      <div className='sendbox'>
-        <input type="text" placeholder='type your message here' onInput={(e) => { 
-          //if((e.target.value) != '') 
-            message = e.target.value;
-        }
-        }/>
-        <button className='sendbtn' onClick = {handleButton}>Send</button>
-      </div>
-    </div>    
-  </>
-  )
-}
-
-function App() {
-  //Login Routing
+function App() {  
   return <LoginPage/>
 }
 export default App
