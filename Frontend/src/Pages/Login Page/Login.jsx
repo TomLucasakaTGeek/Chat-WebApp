922d823b707811b770ad3b9b6a961aa19f623072
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-03-27 20:17:48 +0530

    Trying to fix css

--- Frontend/src/Pages/Login Page/Login.jsx
+++ Frontend/src/Pages/Login Page/Login.jsx
@@ -5,8 +5,8 @@ export function Login() {
         <body id='body'>
           <header id='head'>Welcome Back</header>
           <div className='submit'>
-            <input type="text" placeholder='username' className='box'/>
-            <input type="text" placeholder='password'className='box'/>
+            <input type="text" placeholder=' Username' className='box'/>
+            <input type="text" placeholder=' Password' className='box'/>
             <button className='btn'>LOGIN</button>
             <p id='desc'>Not Registered? <b>Create an Account</b></p>
           </div>
--- Frontend/src/Pages/Signup Page/Signup.jsx
+++ Frontend/src/Pages/Signup Page/Signup.jsx
@@ -4,13 +4,12 @@ export function Signup() {
     
     return (
         <body id='body'>
-          <header id='head'>Welcome Back</header>
+          <header id='head'>Hello Friend</header>
           <div className='submit'>
-            <input type="text" placeholder='Name' className='box'/>
-            <input type="text" placeholder='Email' classname='box' />
-            <input type="text" placeholder='password'className='box'/>
-            <button className='btn'>SIGN UP</button>
-            <p id='desc'>Not Registered? <b>Create an Account</b></p>
+            <input type='text' placeholder=' Name' className='box' />
+            <input type='text' placeholder=' Email' classname='box' />
+            <input type="text" placeholder=' Password' className='box' />
+            <button className='btn'>CONFIRM</button>
           </div>
         </body>
       )
