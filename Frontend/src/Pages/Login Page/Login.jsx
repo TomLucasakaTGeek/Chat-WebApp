b4199ba66656a147a48a6abd30a7e588eb9a9a42
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-03-31 15:55:11 +0530

    fixing the color of link

--- Frontend/src/Pages/Login Page/Login.jsx
+++ Frontend/src/Pages/Login Page/Login.jsx
@@ -6,15 +6,17 @@ export function Login() {
         <body id='body'>
           <header id='head'>Welcome Back</header>
           <div className='submit'>
+            
             <input type="text" placeholder=' Username' className='box'/>
             <input type="text" placeholder=' Password' className='box'/>
             <button className='btn'>LOGIN</button>
 
-            <p className='desc'>Not Registered? 
+            <p className='desc'>Not Registered?  
               <NavLink className='desc' to='/signup'>
                 <b>Create an Account</b>
               </NavLink>
             </p>
+
           </div>
         </body>
     )
